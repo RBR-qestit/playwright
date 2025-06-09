6be353ea496cc0b8ad55b40bd97487a379b0f471
RBR-qestit <rbr@qestit.com>
2025-06-09 14:25:52 +0200

    this commit shouldn't merge

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -13,6 +13,6 @@ test.describe("Connection", () => {
     await page
       .getByRole("textbox", { name: "What needs to be done?" })
       .press("Enter");
-    await expect(page.getByTestId("todo-title")).toContainText("Add note");
+    await expect(page.getByTestId("todo-title")).toContainText("sdfsdfsdf");
   });
 });
