b50538c8121ae3cb712932983f4735a220291bce
RBR-qestit <rbr@qestit.com>
2025-06-09 10:38:40 +0200

    project init

---
+++ tests/test.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("Connection", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("https://demo.playwright.dev/todomvc");
+  });
+
+  test("test", async ({ page }) => {
+    await page.getByRole("textbox", { name: "What needs to be done?" }).click();
+    await page
+      .getByRole("textbox", { name: "What needs to be done?" })
+      .fill("Add note");
+    await page
+      .getByRole("textbox", { name: "What needs to be done?" })
+      .press("Enter");
+    await expect(page.getByTestId("todo-title")).toContainText("Add note");
+  });
+});
